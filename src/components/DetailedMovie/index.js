e4714d259a271d992da82b629e3bc79f3fa34d48
Yoann Moise <myoann@users.noreply.github.com>
2019-11-03 09:50:59 +0100

    Add detailed movie data to the component DetailedMovie

--- src/components/DetailedMovie/index.js
+++ src/components/DetailedMovie/index.js
@@ -85,12 +85,88 @@ class DetailedMovie extends Component {
         <div className="detailedMovieFooter__metadata">
           <section className="detailedMovieFooter__metadata__section">
             <h3>Information</h3>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Tagline
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.tagline}
+              </span>
+            </div>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Genres
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.genres.map(genre => (
+                  <div key={genre.id}>{genre.name}</div>
+                ))}
+              </span>
+            </div>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Release Year
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {new Date(media.release_date).getFullYear()}
+              </span>
+            </div>
           </section>
+
           <section className="detailedMovieFooter__metadata__section">
-            Languages
+            <h3>Details</h3>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Language
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.spoken_languages[0].name}
+              </span>
+            </div>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Runtime
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.runtime}
+              </span>
+            </div>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Also known as
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.original_title}
+              </span>
+            </div>
           </section>
+
           <section className="detailedMovieFooter__metadata__section">
-            Test
+            <h3>Box Office</h3>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Budget
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.budget.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")} $
+              </span>
+            </div>
+
+            <div className="detailedMovieFooter__metadata__section__category">
+              <span className="detailedMovieFooter__metadata__section__category__title">
+                Revenue
+              </span>
+              <span className="detailedMovieFooter__metadata__section__category__value">
+                {media.revenue.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")} $
+              </span>
+            </div>
           </section>
         </div>
       </div>
